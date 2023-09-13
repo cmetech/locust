02e9db46451ffd6b7981153724ffb8895a2a33f7
Corey Ellis <corey@cmetech.io>
2023-09-13 09:16:10 -0400

    updates for custom locust ui

---
+++ locustfile.py
@@ -0,0 +1,19 @@
+import time
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def todos(self):
+        self.client.get("https://jsonplaceholder.typicode.com/todos/1")
+
+    # @task(3)
+    # def view_items(self):
+    #     for item_id in range(10):
+    #         self.client.get(f"/item?id={item_id}", name="/item")
+    #         time.sleep(1)
+
+    # def on_start(self):
+    #     self.client.post("/login", json={"username":"foo", "password":"bar"})
